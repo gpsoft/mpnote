dd49518420cfdbe6963e99a5740c6be6598b573f
gpsoft <etoh.gpsoft@gmail.com>
2021-06-26 11:19:35 +0900

    Draw keyboard and timeline

--- src/mpnote/styles.cljs
+++ src/mpnote/styles.cljs
@@ -1,10 +1,17 @@
 (ns mpnote.styles
   (:require-macros
-    [garden.def :refer [defcssfn]])
+    [garden.def :refer [defcssfn]]
+    garden.selectors)
   (:require
     [spade.core   :refer [defglobal defclass]]
     [garden.units :refer [deg px]]
     [garden.color :refer [rgba]]))
+
+(def color-main-text  "hsl(24deg 90% 20%)")
+(def color-light-main "hsl(24deg 70% 90%)")
+(def color-white :#f5f5f5)
+(def color-black :#333333)
+(def color-black-timeline :#dddddd)
 
 (defcssfn linear-gradient
  ([c1 p1 c2 p2]
@@ -14,15 +21,63 @@
 
 (defglobal defaults
   [:body
-   {:color               :red
-    :background-color    :#ddd
-    :background-image    [(linear-gradient :white (px 2) :transparent (px 2))
-                          (linear-gradient (deg 90) :white (px 2) :transparent (px 2))
-                          (linear-gradient (rgba 255 255 255 0.3) (px 1) :transparent (px 1))
-                          (linear-gradient (deg 90) (rgba 255 255 255 0.3) (px 1) :transparent (px 1))]
-    :background-size     [[(px 100) (px 100)] [(px 100) (px 100)] [(px 20) (px 20)] [(px 20) (px 20)]]
-    :background-position [[(px -2) (px -2)] [(px -2) (px -2)] [(px -1) (px -1)] [(px -1) (px -1)]]}])
+   {:color               color-main-text
+    :background-color    color-light-main
+    :font-size :16px
+    :padding :0.5rem
+    ; :background-image    [(linear-gradient :white (px 2) :transparent (px 2))
+    ;                       (linear-gradient (deg 90) :white (px 2) :transparent (px 2))
+    ;                       (linear-gradient (rgba 255 255 255 0.3) (px 1) :transparent (px 1))
+    ;                       (linear-gradient (deg 90) (rgba 255 255 255 0.3) (px 1) :transparent (px 1))]
+    ; :background-size     [[(px 100) (px 100)] [(px 100) (px 100)] [(px 20) (px 20)] [(px 20) (px 20)]]
+    ; :background-position [[(px -2) (px -2)] [(px -2) (px -2)] [(px -1) (px -1)] [(px -1) (px -1)]]
+    }]
+  [:.main-container
+   {:display :flex}
+   [:.indicator-col
+    {:width "80px"
+     ; :height "100vh"
+     ; :background-color color-white
+     }]
+   [:.annotation-col
+    {:width "80px"
+     ; :height "100vh"
+     ; :background-color :red
+     }]
+   [:.main-col
+    {:flex-grow 1
+     ; :height "100vh"
+     ; :background-color :blue
+     }
+    [:.keys-88 :.timeline
+     {:display :flex}
+     [:.keys-12 {:flex-grow 12}]
+     [:.keys-3 {:flex-grow 3}]
+     [:.keys-1 {:flex-grow 1}]]
+    [:.keys-88
+     {:height :10vh}]
+    [:.timeline
+     {:height :75vh
+      :position :relative}]
 
-(defclass level1
+    [:.octave
+     {:display :flex
+      :border "1px solid #666666"}
+     [:.key-1
+      {:flex-grow 1
+       :position :relative}]
+     [(garden.selectors/+ :.key-1 :.key-1)
+      {:border-left "1px solid #dddddd"}]]
+    [:.keys-88 {}
+     [:.white-key {:background-color color-white}]
+     [:.black-key {:background-color color-black}]]
+    [:.timeline {}
+     [:.white-key {:background-color color-white}]
+     [:.black-key {:background-color color-black-timeline}]]
+    ]])
+
+(defclass brand
   []
-  {:color :green})
+  {:font-size :1.2rem
+   :margin-bottom :0.5rem
+   :color color-main-text})
--- src/mpnote/views.cljs
+++ src/mpnote/views.cljs
@@ -5,10 +5,66 @@
    [mpnote.subs :as subs]
    ))
 
+(defn key-1 [black-white no]
+  [:div.key-1
+   {:class (str (name black-white) "-key")
+    :data-note-no no}])
+
+(defn keys-3 [no]
+  [:div.octave.keys-3
+   (key-1 :white (+ no 0))
+   (key-1 :black (+ no 1))
+   (key-1 :white (+ no 2))
+   ]
+  )
+
+(defn keys-1 [no]
+  [:div.octave.keys-1
+   (key-1 :white (+ no 0))
+   ]
+  )
+
+(defn keys-12 [no]
+  [:div.octave.keys-12
+   (key-1 :white (+ no 0))
+   (key-1 :black (+ no 1))
+   (key-1 :white (+ no 2))
+   (key-1 :black (+ no 3))
+   (key-1 :white (+ no 4))
+   (key-1 :white (+ no 5))
+   (key-1 :black (+ no 6))
+   (key-1 :white (+ no 7))
+   (key-1 :black (+ no 8))
+   (key-1 :white (+ no 9))
+   (key-1 :black (+ no 10))
+   (key-1 :white (+ no 11))
+   ])
+(defn keys-88 []
+  [:div.keys-88
+   (keys-3 21)
+   (map #(keys-12 (+ (* % 12) 24)) (range 7))
+   (keys-1 108)
+   ])
+
+(defn timeline []
+  [:div.timeline
+   (keys-3 21)
+   (map #(keys-12 (+ (* % 12) 24)) (range 7))
+   (keys-1 108)
+   ])
+
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
-    [:div
-     [:h1
-      {:class (styles/level1)}
-      "Hello from " @name]
+    [:div.app
+     [:header
+      [:h1
+       {:class (styles/brand)}
+       "MPのおと"]]
+     [:div.main-container
+      [:div.indicator-col]
+      [:div.main-col
+       (keys-88)
+       (timeline)]
+      [:div.annotation-col]]
+     #_[:h1 {:class (styles/level1)} "Hello from " @name]
      ]))
