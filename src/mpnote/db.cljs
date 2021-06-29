80b1b98c16e47a7c037cbb0b9d549e98f51358b2
gpsoft <etoh.gpsoft@gmail.com>
2021-06-29 20:15:46 +0900

    Fix long notes

--- src/mpnote/db.cljs
+++ src/mpnote/db.cljs
@@ -3,7 +3,7 @@
 (def initial-scroll-top 20)
 
 (def moonlight
-  {:title "ベートーヴェン - Op.27.No.2 -「月光 - 第1楽章」"
+  {:title "ベートーヴェン - Op.27.No.2 -「月光 - 第1楽章」 Lesson1"
    :url "https://www.youtube.com/watch?v=S73r-xb1d6c"
    :ticks-per-bar 12
 
@@ -104,23 +104,364 @@
     {
      :notes
      [{:note-no 64 :hand :right :finger-no 4}]}
+    {
+     :bar-top? true
+     :pedal :on
+     :notes
+     [
+      {:note-no 33 :hand :left :finger-no 5 :length 6}
+      {:note-no 45 :hand :left :finger-no 1 :length 6}
+      {:note-no 57 :hand :right :finger-no 1}
+      ]}
+    {
+     :notes
+     [{:note-no 61 :hand :right :finger-no 2}]}
+    {
+     :notes
+     [{:note-no 64 :hand :right :finger-no 4}]}
+    {
+     :notes
+     [{:note-no 57 :hand :right :finger-no 1}]}
+    {
+     :notes
+     [{:note-no 61 :hand :right :finger-no 2}]}
+    {
+     :notes
+     [{:note-no 64 :hand :right :finger-no 4}]}
+    {
+     :bar-top? true
+     :pedal :on
+     :notes
+     [
+      {:note-no 30 :hand :left :finger-no 5 :length 6}
+      {:note-no 42 :hand :left :finger-no 1 :length 6}
+      {:note-no 57 :hand :right :finger-no 1}
+      ]}
+    {
+     :notes
+     [{:note-no 62 :hand :right :finger-no 3}]}
+    {
+     :notes
+     [{:note-no 66 :hand :right :finger-no 5}]}
+    {
+     :notes
+     [{:note-no 57 :hand :right :finger-no 1}]}
+    {
+     :notes
+     [{:note-no 62 :hand :right :finger-no 3}]}
+    {
+     :notes
+     [{:note-no 66 :hand :right :finger-no 5}]}
+    {
+     :bar-top? true
+     :pedal :on
+     :notes
+     [
+      {:note-no 32 :hand :left :finger-no 4 :length 6}
+      {:note-no 44 :hand :left :finger-no 1 :length 6}
+      {:note-no 56 :hand :right :finger-no 1}
+      ]}
+    {
+     :notes
+     [{:note-no 60 :hand :right :finger-no 2}]}
+    {
+     :notes
+     [{:note-no 66 :hand :right :finger-no 5}]}
+    {
+     :pedal :on
+     :notes
+     [{:note-no 56 :hand :right :finger-no 1}]}
+    {
+     :notes
+     [{:note-no 61 :hand :right :finger-no 3}]}
+    {
+     :notes
+     [{:note-no 64 :hand :right :finger-no 5}]}
+    {
+     :bar-top? true
+     :pedal :on
+     :notes
+     [
+      {:note-no 32 :hand :left :finger-no 5 :length 6}
+      {:note-no 44 :hand :left :finger-no 12 :length 6}
+      {:note-no 56 :hand :right :finger-no 1}
+      ]}
+    {
+     :notes
+     [{:note-no 61 :hand :right :finger-no 3}]}
+    {
+     :notes
+     [{:note-no 63 :hand :right :finger-no 4}]}
+    {
+     :pedal :on
+     :notes
+     [{:note-no 53 :hand :right :finger-no 1}]}
+    {
+     :notes
+     [{:note-no 60 :hand :right :finger-no 3}]}
+    {
+     :notes
+     [{:note-no 63 :hand :right :finger-no 5}]}
+    {
+     :bar-top? true
+     :pedal :on
+     :notes
+     [
+      {:note-no 37 :hand :left :finger-no 5 :length 12}
+      {:note-no 44 :hand :left :finger-no 2 :length 12}
+      {:note-no 49 :hand :left :finger-no 1 :length 12}
+      {:note-no 52 :hand :right :finger-no 1}
+      ]}
+    {
+     :notes
+     [{:note-no 56 :hand :right :finger-no 2}]}
+    {
+     :notes
+     [{:note-no 61 :hand :right :finger-no 3}]}
+    {
+     :notes
+     [{:note-no 56 :hand :right :finger-no 1}]}
+    {
+     :notes
+     [{:note-no 61 :hand :right :finger-no 2}]}
+    {
+     :notes
+     [{:note-no 64 :hand :right :finger-no 3}]}
+    {
+     :notes
+     [{:note-no 56 :hand :right :finger-no 1}]}
+    {
+     :notes
+     [{:note-no 61 :hand :right :finger-no 2}]}
+    {
+     :notes
+     [{:note-no 64 :hand :right :finger-no 3}]}
+    {
+     :notes
+     [{:note-no 56 :hand :right :finger-no 1}
+      {:note-no 68 :hand :right :finger-no 5 :length 2.5}]}
+    {
+     :notes
+     [{:note-no 61 :hand :right :finger-no 2}]}
+    {
+     :weight 0.5
+     :notes
+     [{:note-no 64 :hand :right :finger-no 3}]}
+    {
+     :weight 0.5
+     :notes
+     [{:note-no 68 :hand :right :finger-no 5 :length 0.5}]}
+    {
+     :bar-top? true
+     :pedal :on
+     :notes
+     [
+      {:note-no 36 :hand :left :finger-no 5 :length 12}
+      {:note-no 44 :hand :left :finger-no 2 :length 12}
+      {:note-no 48 :hand :left :finger-no 1 :length 12}
+      {:note-no 56 :hand :right :finger-no 1}
+      {:note-no 68 :hand :right :finger-no 5 :length 9}
+      ]}
+    {
+     :notes
+     [{:note-no 63 :hand :right :finger-no 2}]}
+    {
+     :notes
+     [{:note-no 66 :hand :right :finger-no 4}]}
+    {
+     :notes
+     [{:note-no 56 :hand :right :finger-no 1}]}
+    {
+     :notes
+     [{:note-no 63 :hand :right :finger-no 2}]}
+    {
+     :notes
+     [{:note-no 66 :hand :right :finger-no 4}]}
+    {
+     :notes
+     [{:note-no 56 :hand :right :finger-no 1}]}
+    {
+     :notes
+     [{:note-no 63 :hand :right :finger-no 2}]}
+    {
+     :notes
+     [{:note-no 66 :hand :right :finger-no 4}]}
+    {
+     :notes
+     [{:note-no 56 :hand :right :finger-no 1}
+      {:note-no 68 :hand :right :finger-no 5 :length 2.5}]}
+    {
+     :notes
+     [{:note-no 63 :hand :right :finger-no 2}]}
+    {
+     :weight 0.5
+     :notes
+     [{:note-no 66 :hand :right :finger-no 3}]}
+    {
+     :weight 0.5
+     :notes
+     [{:note-no 68 :hand :right :finger-no 4 :length 0.5}]}
+    {
+     :bar-top? true
+     :pedal :on
+     :notes
+     [
+      {:note-no 37 :hand :left :finger-no 5 :length 6}
+      {:note-no 49 :hand :left :finger-no 1 :length 6}
+      {:note-no 56 :hand :right :finger-no 1}
+      {:note-no 68 :hand :right :finger-no 5 :length 6}
+      ]}
+    {
+     :notes
+     [{:note-no 61 :hand :right :finger-no 2}]}
+    {
+     :notes
+     [{:note-no 64 :hand :right :finger-no 3}]}
+    {
+     :notes
+     [{:note-no 56 :hand :right :finger-no 1}]}
+    {
+     :notes
+     [{:note-no 61 :hand :right :finger-no 2}]}
+    {
+     :notes
+     [{:note-no 64 :hand :right :finger-no 3}]}
+    {
+     :bar-top? true
+     :pedal :on
+     :notes
+     [
+      {:note-no 30 :hand :left :finger-no 5 :length 6}
+      {:note-no 42 :hand :left :finger-no 1 :length 6}
+      {:note-no 57 :hand :right :finger-no 1}
+      {:note-no 69 :hand :right :finger-no 5 :length 6}
+      ]}
+    {
+     :notes
+     [{:note-no 61 :hand :right :finger-no 2}]}
+    {
+     :notes
+     [{:note-no 66 :hand :right :finger-no 4}]}
+    {
+     :notes
+     [{:note-no 57 :hand :right :finger-no 1}]}
+    {
+     :notes
+     [{:note-no 61 :hand :right :finger-no 2}]}
+    {
+     :notes
+     [{:note-no 66 :hand :right :finger-no 4}]}
+    {
+     :bar-top? true
+     :pedal :on
+     :notes
+     [
+      {:note-no 35 :hand :left :finger-no 5 :length 6}
+      {:note-no 47 :hand :left :finger-no 1 :length 6}
+      {:note-no 56 :hand :right :finger-no 1}
+      {:note-no 68 :hand :right :finger-no 5 :length 6}
+      ]}
+    {
+     :notes
+     [{:note-no 59 :hand :right :finger-no 2}]}
+    {
+     :notes
+     [{:note-no 64 :hand :right :finger-no 4}]}
+    {
+     :notes
+     [{:note-no 56 :hand :right :finger-no 1}]}
+    {
+     :notes
+     [{:note-no 59 :hand :right :finger-no 2}]}
+    {
+     :notes
+     [{:note-no 64 :hand :right :finger-no 4}]}
+    {
+     :bar-top? true
+     :pedal :on
+     :notes
+     [
+      {:note-no 35 :hand :left :finger-no 5 :length 6}
+      {:note-no 47 :hand :left :finger-no 1 :length 6}
+      {:note-no 57 :hand :right :finger-no 1}
+      {:note-no 66 :hand :right :finger-no 5 :length 3}
+      ]}
+    {
+     :notes
+     [{:note-no 59 :hand :right :finger-no 2}]}
+    {
+     :notes
+     [{:note-no 63 :hand :right :finger-no 3}]}
+    {
+     :notes
+     [{:note-no 57 :hand :right :finger-no 1}
+      {:note-no 71 :hand :right :finger-no 5 :length 3}]}
+    {
+     :notes
+     [{:note-no 59 :hand :right :finger-no 1}]}
+    {
+     :notes
+     [{:note-no 63 :hand :right :finger-no 2}]}
+    {
+     :bar-top? true
+     :pedal :on
+     :notes
+     [
+      {:note-no 40 :hand :left :finger-no 5 :length 12}
+      {:note-no 52 :hand :left :finger-no 1 :length 12}
+      {:note-no 56 :hand :right :finger-no 1}
+      {:note-no 64 :hand :right :finger-no 4}
+      ]}
+    {
+     :notes
+     [{:note-no 59 :hand :right :finger-no 2}]}
+    {
+     :notes
+     [{:note-no 64 :hand :right :finger-no 4}]}
+    {
+     :notes
+     [{:note-no 56 :hand :right :finger-no 1}]}
+    {
+     :notes
+     [{:note-no 59 :hand :right :finger-no 2}]}
+    {
+     :notes
+     [{:note-no 64 :hand :right :finger-no 4}]}
+    {
+     :notes
+     [{:note-no 56 :hand :right :finger-no 1}]}
+    {
+     :notes
+     [{:note-no 59 :hand :right :finger-no 2}]}
+    {
+     :notes
+     [{:note-no 64 :hand :right :finger-no 4}]}
+    {
+     :notes
+     [{:note-no 56 :hand :right :finger-no 1}]}
+    {
+     :notes
+     [{:note-no 59 :hand :right :finger-no 2}]}
+    {
+     :notes
+     [{:note-no 64 :hand :right :finger-no 4}]}
     ]
    })
 
 (defn long-note
-  [{:keys [length] :or {length 1}}]
-  (> length 1))
+  [weight {:keys [length] :or {length 1}}]
+  (> length weight))
 
 (defn long-notes
   [steps]
-  (mapcat (fn [{:keys [step-ix tickstamp notes]}]
-            (->> (filter long-note notes)
+  (mapcat (fn [{:keys [step-ix tickstamp weight notes]}]
+            (->> (filter #(long-note weight %) notes)
                 (map #(assoc % :tickstamp tickstamp)) ))
           steps))
 
 (defn in-step
-  [tick {:keys [tickstamp length]}]
-  (and (> tick tickstamp) (< tick (+ tickstamp length))))
+  [tick {:keys [tickstamp length] :or {length 1}}]
+  (< tickstamp tick (+ tickstamp length)))
 
 (defn make-dummy-note
   [{:keys [note-no hand]}]
@@ -128,16 +469,18 @@
    :note-no note-no
    :hand hand})
 
-(defn a
+(defn make-dummy-notes
   [lnotes {:keys [tickstamp notes] :as step}]
+  (do (js/console.log tickstamp))
   (let [notes-for-step (filter #(in-step tickstamp %) lnotes)
+        _ (do (js/console.log (:step-ix step)) (js/console.log (clj->js notes-for-step)))
         notes-for-step (map make-dummy-note notes-for-step)]
     (assoc step :notes (concat notes notes-for-step))))
 
 (defn append-dummy-notes
   [steps]
   (let [lnotes (long-notes steps)]
-    (map #(a lnotes %) steps)))
+    (map #(make-dummy-notes lnotes %) steps)))
 
 (comment
   (let [steps (:steps moonlight)
@@ -150,6 +493,7 @@
   [[steps ix tick] {:keys [weight] :or {weight 1} :as step}]
   (let [step (assoc step
                     :step-ix ix
+                    :weight weight
                     :tickstamp tick)]
     [(conj steps step)
      (inc ix)
